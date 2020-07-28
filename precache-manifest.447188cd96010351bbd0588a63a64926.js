883cad7db1f46d264f95d608ebe6507322ef846e
Ali Nazari <ali.nazari@aut.ac.ir>
2020-07-28 16:49:39 +0430

    Updates

---
+++ precache-manifest.447188cd96010351bbd0588a63a64926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66e9bbd5b25671edc57f97bf60a5892",
+    "url": "/To-Do/index.html"
+  },
+  {
+    "revision": "18eece05d639a7bd8569",
+    "url": "/To-Do/static/css/main.b74ecb69.chunk.css"
+  },
+  {
+    "revision": "93da2e4524f50af46c1c",
+    "url": "/To-Do/static/js/2.76455d49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do/static/js/2.76455d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18eece05d639a7bd8569",
+    "url": "/To-Do/static/js/main.b1234d3b.chunk.js"
+  },
+  {
+    "revision": "220b210080f13003b93e",
+    "url": "/To-Do/static/js/runtime-main.7c25e568.js"
+  }
+]);
